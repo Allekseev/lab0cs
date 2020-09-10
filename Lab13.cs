e1f51a5cff0bd7dcae08cb2aa57ed6176036d066
Allekseev <55903749+Allekseev@users.noreply.github.com>
2020-09-10 23:06:42 +0500

    Update Lab13.cs

--- Lab13.cs
+++ Lab13.cs
@@ -8,8 +8,8 @@ namespace Lab13
         {
             int sum = 0;
             for (int i = 22; i < 100; i += 2)
-                if (i % 10 != 0 && i % 3 == 0)
-                    sum+=i;
+                if (i % 10 != 0 && i % 10 != 6 && i % 3 == 0)
+                    sum += i;
             Console.WriteLine($"сумма равна {sum}");
         }
     }
